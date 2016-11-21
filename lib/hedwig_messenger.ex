fa60f92eb004b1c074b762fccea05d44d5dd5be0
Erik Nilsen <enilsen16@live.com>
2016-11-20 17:03:59 -0800

    initial adapter

--- lib/hedwig_messenger.ex
+++ lib/hedwig_messenger.ex
@@ -1,18 +1,58 @@
-defmodule HedwigMessenger do
-  @moduledoc """
-  Documentation for HedwigMessenger.
-  """
+defmodule Hedwig.Adapters.Messenger do
+  use Hedwig.Adapter
+  require Logger
 
-  @doc """
-  Hello world.
+  def init({robot, opts}) do
+    HTTPoison.start
+    :global.register_name({__MODULE__}, opts[:name], self())
+    Hedwig.Robot.handle_connect(robot)
 
-  ## Examples
+    state = %{
+      robot: robot
+    }
 
-      iex> HedwigMessenger.hello
-      :world
+    {:ok, robot}
+  end
 
-  """
-  def hello do
-    :world
+  def handle_cast({_, msg}, state) do
+    send_text_message(state.user_id, msg, state)
+    {:noreply, state}
+  end
+
+  def handle_call(:robot, _, %{robot: robot} = state) do
+    {:reply, robot, state}
+  end
+
+  def send_text_message(user_id, msg, state) do
+    endpoint = "https://graph.facebook.com/v2.6/me/messages?access_token=#{Application.get_env("PAGE_ACCESS_TOKEN")}"
+    body = build_body(:text, msg, state.user_id)
+    Logger.info "sending #{body} to #{user_id}"
+
+    case send_request(endpoint, user_id, body, state) do
+      {:ok, %HTTPoison.Response{status_code: status_code} = response } when status_code in 200..299 ->
+        Logger.info("#{inspect response}")
+
+      {:ok, %HTTPoison.Response{status_code: status_code} = response } when status_code in 400..599 ->
+        Logger.error("#{inspect response}")
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.error("#{inspect reason}")
+    end
+  end
+
+  defp send_request(url, user_id, body, state) do
+    headers = [{"Content-Type", "application/json" }]
+    HTTPoison.post(url, body, headers)
+  end
+
+  defp build_body(:text, text, user_id) do
+    %{
+      recipient: %{
+        id: user_id
+      },
+      message: %{
+        text: text
+      }
+    }
   end
 end
